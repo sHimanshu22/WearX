8733d4c76d8fa9715cc5d33851efbb88469a82e5
sHimanshu22 <himanshusonawane022@gmail.com>
2026-03-12 22:58:44 +0530

    Added Cart handelling routes

--- backend/routes/CartRoutes.js
+++ backend/routes/CartRoutes.js
@@ -139,4 +139,131 @@ router.put("/", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route DELETE /api/cart
+// @desc Remove a product from a cart
+// @access Public
+
+router.delete("/", async (req, res) => {
+  const { productId, size, color, guestId, userId } = req.body;
+  try {
+    let cart = await getCart(userId, guestId);
+    if (!cart) {
+      return res.status(404).json({ message: "cart not Found" });
+    }
+
+    const productIndex = cart.products.findIndex(
+      (p) =>
+        p.productId.toString() === productId &&
+        p.size === size &&
+        p.color === color,
+    );
+
+    if (productIndex > -1) {
+      cart.products.splice(productIndex, 1);
+
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0,
+      );
+      await cart.save();
+      return res.status(200).json(cart);
+    } else {
+      return res.status(404).json({ message: "Product Not Found In the Cart" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+// @route GET /api/cart
+// @desc Get logged-in user's or guest user's cart
+// @access Public
+
+router.get("/", async (req, res) => {
+  const { userId, guestId } = req.query;
+  try {
+    const cart = await getCart(userId, guestId);
+    if (cart) {
+      res.json(cart);
+    } else {
+      res.status(404).json({ messgae: "Cart Not Found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+// @route POST /api/cart/merge
+// @desc Merge guest cart into user cart on  login
+// @access Private
+
+router.post("/merge", protect, async (req, res) => {
+  const { guestId } = req.body;
+
+  try {
+    // Find the Guest cart and User Cart
+    const guestCart = await Cart.findOne({ guestId });
+    const userCart = await Cart.findOne({ user: req.user._id });
+
+    if (guestCart) {
+      if (guestCart.products.length === 0) {
+        return res.status(400).json({ messgae: "Guest cart is Empty" });
+      }
+
+      if (userCart) {
+        //Merge guest cart into user cart
+        guestCart.products.forEach((guestItem) => {
+          const productIndex = userCart.products.findIndex((item) => {
+            item.productId.toString() === guestItem.productId.toString() &&
+              item.size === guestItem.size &&
+              item.color === guestItem.color;
+          });
+
+          if (productIndex > -1) {
+            // If the items exists in the user cart , update the Quantity
+            userCart.products[productIndex].quantity += guestItem.quantity;
+          } else {
+            // Otherwise , add the quest item to the cart
+            userCart.products.push(guestItem);
+          }
+        });
+
+        userCart.totalPrice = userCart.products.reduce((acc, item) => {
+          (acc + item.price * item.quantity, 0);
+        });
+
+        await userCart.save();
+
+        //remove the guest cart after merging
+        try {
+          await Cart.findOneAndDelete({ guestId });
+        } catch (error) {
+          console.error("Error deleting guest cart : ", error);
+          res.status(200).json(userCart);
+        }
+      } else {
+        // if user has no exixting cart , assign the guest cart to the user
+        guestCart.user = req.user._id;
+        guestCart.guestId = undefined;
+
+        await guestCart.save();
+
+        res.status(200).json(guestCart);
+      }
+    } else {
+      if (userCart) {
+        // Guest cart has alredy been merged, return user cart
+        return res.status(200).json(userCart);
+      }
+      res.status(404).json({ message: "Guest Cart not Found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: " server Error" });
+  }
+});
+
 module.exports = router;
